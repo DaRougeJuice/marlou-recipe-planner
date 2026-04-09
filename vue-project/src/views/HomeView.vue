3664a57e3f881786022b2adb380a520f8e705ac9
Marlou Pamintuan <marloupam29@gmail.com>
2026-04-09 22:06:03 +0800

    setup vue, tailwind, and router

---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-8">
+    <h1 class="text-3xl font-bold">Welcome to Marlou's Recipe Planner 🍳</h1>
+    <p class="mt-2 text-gray-500">Start by searching for recipes!</p>
+  </div>
+</template>
