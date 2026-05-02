7f0e29853dbfb5c36407efa17c249f987148298d
Marlou Pamintuan <marloupam29@gmail.com>
2026-05-02 22:46:22 +0800

    fix Dockerfile location

--- vue-project/src/axios.js
+++ vue-project/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000'
 axios.defaults.withCredentials = false
 
 axios.interceptors.request.use(config => {
