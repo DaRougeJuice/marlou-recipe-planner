8e6baf5063505bc0562bc318d4c83312e357a8d9
Marlou Pamintuan <marloupam29@gmail.com>
2026-05-03 00:50:59 +0800

    fix cors config

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
-    'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => false,
+    "paths" => ["api/*"],
+    "allowed_methods" => ["*"],
+    "allowed_origins" => [env("CORS_ALLOWED_ORIGINS", "http://localhost:5173")],
+    "allowed_headers" => ["*"],
+    "exposed_headers" => [],
+    "max_age" => 0,
+    "supports_credentials" => false,
 ];
